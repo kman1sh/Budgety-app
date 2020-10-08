eed5db0c24b1914be50fd5a73ab6ff1ddd11ce7d
Manish Kumar <67220364+kman1sh@users.noreply.github.com>
2020-10-08 13:54:35 +0530

    month display fix

--- app.js
+++ app.js
@@ -336,7 +336,7 @@ var UIController = (function () {
          var months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
          month = now.getMonth();
          year = now.getFullYear();
-         document.querySelector(DOMStrings.dateLable).textContent = months[month - 1] + ", " + year;
+         document.querySelector(DOMStrings.dateLable).textContent = months[month] + ", " + year;
 
 
       },
